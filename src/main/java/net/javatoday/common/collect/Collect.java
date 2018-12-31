1f9d82e99b2353defcf21f07e42eea7e0dba5860
zhenya <leonov@gmail.com>
2018-12-30 23:05:41 -0800

    Combined static collection utilities into Collect

--- src/main/java/net/javatoday/common/collect/Collect.java
+++ src/main/java/net/javatoday/common/collect/Collect.java
@@ -18,26 +18,70 @@ package net.javatoday.common.collect;
 import static com.google.common.base.Preconditions.checkElementIndex;
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Objects;
+import java.util.Queue;
 import java.util.RandomAccess;
 import java.util.function.Predicate;
 
+import com.google.common.collect.Collections2;
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
+import com.google.common.collect.Iterators;
 
 /**
- * More static utility methods which operate on or return {@link List}s.
+ * Static utility methods for working with {@code Collection}s.
  * 
  * @author Zhenya Leonov
- * @see Lists
+ * @see Collections
+ * @see Collections2
  */
-public class MoreLists {
+final public class Collect {
 
-    private MoreLists() {
+    private Collect() {
     }
 
+    /**
+     * Returns {@code true} if a collection contains all of the specified elements.
+     * 
+     * @param c        the collection
+     * @param elements the specified elements
+     * @return {@code true} if a collection contains all of the specified elements, {@code false otherwise}
+     */
+    public static boolean containsAll(final Collection<?> c, final Iterable<?> elements) {
+        checkNotNull(c, "c == null");
+        checkNotNull(elements, "elements == null");
+        return elements instanceof Collection ? c.containsAll((Collection<?>) elements) : containsAll(c, elements.iterator());
+    }
+
+    /**
+     * Returns {@code true} if a collection contains all of the specified elements.
+     * <p>
+     * Guava equivalent: {@code Iterators.all(Iterator, Predicates.in(Collection))}
+     * 
+     * @param c        the collection
+     * @param elements the specified elements
+     * @return {@code true} if a collection contains all of the specified elements, {@code false otherwise}
+     */
+    public static boolean containsAll(final Collection<?> c, final Iterator<?> elements) {
+        checkNotNull(c, "c == null");
+        checkNotNull(elements, "elements == null");
+
+        try {
+            while (elements.hasNext())
+                if (!c.contains(elements.next()))
+                    return false;
+        } catch (final ClassCastException e) {
+            return false;
+        } catch (final NullPointerException e) {
+            return false;
+        }
+        return true;
+    }
+    
     /**
      * Returns the index of the first occurrence of the specified element in the given list, starting the search at
      * {@code fromIndex}, or -1 if there is no such element.
@@ -178,6 +222,78 @@ public class MoreLists {
         }
 
         return -1;
+    }
+
+    /**
+     * Attempts to insert the given elements into the specified queue. When using a capacity-restricted queue, some or all
+     * of the elements maybe rejected.
+     * <p>
+     * This method is generally preferable to {@link Collections#addAll(Collection, Object...) Collections.addAll(Queue,
+     * E...)}, which can fail to insert an element only by throwing an exception.
+     * 
+     * @param queue    the specified queue
+     * @param elements the elements to insert into the queue
+     * @return {@code true} if at least one elements was inserted into the queue; {@code false} otherwise
+     * @throws ClassCastException       if the class of one or more elements prevents them from being inserted into this
+     *                                  queue
+     * @throws NullPointerException     if one or more elements is {@code null} and this queue does not permit null elements
+     * @throws IllegalArgumentException if some property of one or more element prevents them from being inserted into this
+     *                                  queue
+     */
+    @SafeVarargs
+    public static <E> boolean offerAll(Queue<? super E> queue, E... elements) {
+        checkNotNull(queue, "queue == null");
+        checkNotNull(elements, "elements == null");
+        boolean result = false;
+        for (final E e : elements)
+            result |= queue.offer(e);
+        return result;
+    }
+
+    /**
+     * Attempts to insert the given elements into the specified queue. When using a capacity-restricted queue, some or all
+     * of the elements maybe rejected.
+     * <p>
+     * This method is generally preferable to {@link Iterables#addAll(Collection, Iterable) Iterables.addAll(Queue,
+     * Iterable)}, which can fail to insert an element only by throwing an exception.
+     * 
+     * @param queue    the specified queue
+     * @param elements the elements to insert into the queue
+     * @return {@code true} if at least one elements was inserted into the queue; {@code false} otherwise
+     * @throws ClassCastException       if the class of one or more elements prevents them from being inserted into this
+     *                                  queue
+     * @throws NullPointerException     if one or more elements is {@code null} and this queue does not permit null elements
+     * @throws IllegalArgumentException if some property of one or more element prevents them from being inserted into this
+     *                                  queue
+     */
+    public static <E> boolean offerAll(Queue<? super E> queue, Iterable<E> elements) {
+        checkNotNull(elements, "elements == null");
+        return offerAll(queue, elements.iterator());
+    }
+
+    /**
+     * Attempts to insert the given elements into the specified queue. When using a capacity-restricted queue, some or all
+     * of the elements maybe rejected.
+     * <p>
+     * This method is generally preferable to {@link Iterators#addAll(Collection, Iterator) Iterators.addAll(Queue,
+     * Iterator)}, which can fail to insert an element only by throwing an exception.
+     * 
+     * @param queue    the specified queue
+     * @param elements the elements to insert into the queue
+     * @return {@code true} if at least one elements was inserted into the queue; {@code false} otherwise
+     * @throws ClassCastException       if the class of one or more elements prevents them from being inserted into this
+     *                                  queue
+     * @throws NullPointerException     if one or more elements is {@code null} and this queue does not permit null elements
+     * @throws IllegalArgumentException if some property of one or more element prevents them from being inserted into this
+     *                                  queue
+     */
+    public static <E> boolean offerAll(Queue<? super E> queue, Iterator<E> elements) {
+        checkNotNull(queue, "queue == null");
+        checkNotNull(elements, "elements == null");
+        boolean result = false;
+        while (elements.hasNext())
+            result |= queue.offer(elements.next());
+        return result;
     }
 
 }
